c0f2239d51632b39baa5effa1e576768eb06b760
Yogesh Thambidurai <gyogesh2015@outlook.com>
2024-04-30 20:32:34 -0400

    feat: add messages route

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
-from flask import Flask
+from collections import OrderedDict
+
+from flask import Flask, request
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+next_id = 1
+messages = OrderedDict()
 
-if __name__ == '__main__':
+
+@app.route("/messages", methods=["GET", "POST", "DELETE"])
+def messages():
+    global messages, next_id
+
+    match request.method:
+        case "GET":
+            return messages, 200
+        case "POST" if len(request.data) > 50:
+            # 413 Request Entity Too Large
+            return "Message can only be up to 50 characters", 413
+        case "POST":
+            messages[next_id] = request.data.decode("utf-8")
+            if len(messages) > 50:
+                messages.popitem(last=False)
+            next_id += 1
+            return str(next_id - 1), 201
+        case "DELETE" if request.data in messages.keys():
+            messages.pop(request.data), 204
+        case "DELETE":
+            return "Message ID not found", 404
+
+
+if __name__ == "__main__":
     app.run()
